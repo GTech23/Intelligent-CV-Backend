56be309c75ffac0528b733d4cc44df77f80e3849
Godstime Pious <141561131+GTech23@users.noreply.github.com>
2025-09-15 14:58:26 +0000

    Update on Template CRUD

--- src/controllers/template.controller.js
+++ src/controllers/template.controller.js
@@ -1,1 +1,17 @@
-import ResumeTemplate from "../models/ResumeTemplate";
+
+import ResumeTemplate from "../models/ResumeTemplate.js";
+
+export async function createTemplate(req, res,){
+    const body = req.body;
+    const newTemplate = new ResumeTemplate({...body});
+    await newTemplate.save()
+    res.status(201).json({success: true, message: `Template created`})
+}
+
+
+export async function getTemplates(req, res) {
+    const templates = await ResumeTemplate.find();
+    if(templates.length === 0 || !templates) return res.status(404).json({message: `Templates not found`});
+
+    res.status(200).json({success: true, templates})
+}
