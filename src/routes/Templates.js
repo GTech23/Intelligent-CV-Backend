4dc3d991502108f218d9a33003cb02251a58f1d4
Godstime Pious <godstimepious33@gmail.com>
2025-09-25 14:50:47 +0100

    Remove authorization from on getTemplates

--- src/routes/Templates.js
+++ src/routes/Templates.js
@@ -1,13 +1,17 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import { createTemplate, deleteTemplate, getTemplates } from '../controllers/template.controller.js';
-import authorizeAuth from '../middlewares/authorize.js';
-import { authorizeAdmin } from '../middlewares/authorizeAdmin.js';
+import express from "express";
+import mongoose from "mongoose";
+import {
+  createTemplate,
+  deleteTemplate,
+  getTemplates,
+} from "../controllers/template.controller.js";
+import authorizeAuth from "../middlewares/authorize.js";
+import { authorizeAdmin } from "../middlewares/authorizeAdmin.js";
 
 const templateRouter = express.Router();
 
-templateRouter.post('/create', authorizeAuth, authorizeAdmin, createTemplate );
-templateRouter.get('/', authorizeAuth, authorizeAdmin, getTemplates );
-templateRouter.delete('/:id', authorizeAuth, authorizeAdmin, deleteTemplate)
+templateRouter.post("/create", authorizeAuth, authorizeAdmin, createTemplate);
+templateRouter.get("/", getTemplates);
+templateRouter.delete("/:id", authorizeAuth, authorizeAdmin, deleteTemplate);
 
 export default templateRouter;
