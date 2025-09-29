a7ce2f71b38458953fc167c6a50910953eba12dc
Godstime Pious <godstimepious33@gmail.com>
2025-09-29 18:51:25 +0100

    Update Origin for cors

--- src/config/corsConfig.js
+++ src/config/corsConfig.js
@@ -1,6 +1,10 @@
 import cors from "cors";
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://intelligent-cv-backend.onrender.com",
+];
 
 const corsOptions = {
   origin: (origin, callback) => {
