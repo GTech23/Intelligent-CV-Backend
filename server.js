91b3c208412748fbec15947d535e240c15d8fa9c
Godstime Pious <godstimepious33@gmail.com>
2025-09-29 18:41:40 +0100

    Setup CORS Configurations

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 import express from "express";
 import { config } from "dotenv";
-import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import connectDB from "./src/config/db.js";
-import { logger } from "./src/middlewares/logger.js";
 import exphbs from "express-handlebars";
 import path from "path";
 import url from "url";
@@ -15,6 +13,7 @@ const PORT = process.env.PORT;
 import authRouter from "./src/routes/Auth.js";
 import resumeRouter from "./src/routes/Resume.js";
 import templateRouter from "./src/routes/Templates.js";
+import corsOptions from "./src/config/corsConfig.js";
 
 const app = express();
 await connectDB();
@@ -35,7 +34,8 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use(cors());
+
+app.use(corsOptions);
 app.use(cookieParser());
 app.use(express.json());
 // app.use(logger);
---
+++ src/config/corsConfig.js
@@ -0,0 +1,19 @@
+import cors from "cors";
+
+const allowedOrigins = ["http://localhost:3000"];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS: Unauthorized domain"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+export default cors(corsOptions);
